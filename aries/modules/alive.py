01059d2214b34ca0718093ebcc92125a510b06d7
CrePavan <93823376+CrePavan@users.noreply.github.com>
2021-12-22 09:02:34 +0530

    Update alive.py

--- aries/modules/alive.py
+++ aries/modules/alive.py
@@ -9,15 +9,15 @@ PHOTO = "https://telegra.ph/file/c20a6da05abc7a15297d1.jpg"
 @register(pattern=("/alive|/ALIVE"))
 async def awake(event):
     event.sender.first_name
-    ARIES = "**Hello im Creator Pavan** \n\n"
+    ARIES = "**Hello I'm Creator Pavan** \n\n"
     ARIES += "**ALL SYSTEM WORKING PROPERLY**\n\n"
     ARIES += " ⎇ **Python :** __3.9.7__ \n\n"
     ARIES += " ⎇ **Pyrogram :** __1.2.9__ \n\n"
     ARIES += " ⎇ **MongoDB :** __2.5.1__ \n\n"
     ARIES += " ⎇ **Platform :** __linux__ \n\n"
-    ARIES += " ⎇ **My Lord** : [Creator Pavan](https://t.me/Creator_pavan) ☠⌉\n\n"
-    ARIES += " ⎇ **Creator Pavan Robot** ⌉\n\n"
-    ARIES += " ⎇ **TELETHON : 6.6.6 Latest** ⌉\n\n"
+    ARIES += " ⎇ **My Lord** : [Creator Pavan](https://t.me/Creator_pavan)\n\n"
+    ARIES += " ⎇ **Creator Pavan Robot**\n\n"
+    ARIES += " ⎇ **TELETHON : 6.6.6 Latest**\n\n"
     ARIES += " |||| || ||| |||| || |||||| ||||| || || ||"
     BUTTON = [
         [
