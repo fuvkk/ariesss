8b4159bf19c6a56a5b696ccfe9f2b12611ce8619
UNREALBOT12 <91039807+UNREALBOT12@users.noreply.github.com>
2021-12-22 14:11:15 +0530

    Update notes.py

--- aries/modules/notes.py
+++ aries/modules/notes.py
@@ -574,7 +574,7 @@ A button can be added to a note by using standard markdown link syntax - the lin
 
 """
 
-__mod_name__ = "🔘 Notes"
+__mod_name__ = "ɴᴏᴛᴇ"
 
 GET_HANDLER = CommandHandler("get", cmd_get, run_async=True)
 HASH_GET_HANDLER = MessageHandler(Filters.regex(r"^#[^\s]+"), hash_get, run_async=True)
