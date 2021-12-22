904715e2acccd5a74d98294afc3cc56d3a6d5459
CrePavan <93823376+CrePavan@users.noreply.github.com>
2021-12-22 07:07:56 +0530

    Update misc.py

--- aries/modules/helper_funcs/misc.py
+++ aries/modules/helper_funcs/misc.py
@@ -71,7 +71,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
             ]
         )
 
-    pairs = [modules[i * 4 : (i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)]
+    pairs = [modules[i * 3 : (i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)]
 
     round_num = len(modules) / 3
     calc = len(modules) - round(round_num)
@@ -83,7 +83,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
 
     # can only have a certain amount of buttons side by side
     if len(pairs) > 3:
-        pairs = pairs[modulo_page * 4 : 3 * (modulo_page + 1)] + [
+        pairs = pairs[modulo_page * 3 : 3 * (modulo_page + 1)] + [
             (
                 EqInlineKeyboardButton(
                     "⌫", callback_data="{}_prev({})".format(prefix, modulo_page)
