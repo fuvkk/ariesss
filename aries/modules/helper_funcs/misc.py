5cf02fda312ee39c5bed1a29b022e98edfffe559
CrePavan <93823376+CrePavan@users.noreply.github.com>
2021-12-22 07:30:19 +0530

    Update misc.py

--- aries/modules/helper_funcs/misc.py
+++ aries/modules/helper_funcs/misc.py
@@ -71,24 +71,24 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
             ]
         )
 
-    pairs = [modules[i * 3 : (i - 1) * 3] for i in range((len(modules) + 3 - 1) // 3)]
+    pairs = [modules[i * 3 : (i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)]
 
-    round_num = len(modules) / 3
+    round_num = len(modules) / 4
     calc = len(modules) - round(round_num)
     if calc in [1, 2]:
         pairs.append((modules[-1],))
 
-    max_num_pages = ceil(len(pairs) / 3)
+    max_num_pages = ceil(len(pairs) / 4)
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
-    if len(pairs) > 3:
-        pairs = pairs[modulo_page * 3 : 4 * (modulo_page - 1)] + [
+    if len(pairs) > 4:
+        pairs = pairs[modulo_page * 3 : 3 * (modulo_page + 1)] + [
             (
                 EqInlineKeyboardButton(
                     "⌫", callback_data="{}_prev({})".format(prefix, modulo_page)
                 ),
-                EqInlineKeyboardButton("⌂", callback_data="aboutmanu_"),
+                EqInlineKeyboardButton("ʜᴏᴍᴇ", callback_data="aboutmanu_"),
                 EqInlineKeyboardButton(
                     "⌦", callback_data="{}_next({})".format(prefix, modulo_page)
                 ),
@@ -96,7 +96,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         ]
 
     else:
-        pairs += [[EqInlineKeyboardButton("⌂", callback_data="aboutmanu_")]]
+        pairs += [[EqInlineKeyboardButton("ʜᴏᴍᴇ", callback_data="aboutmanu_")]]
 
     return pairs
 
