bc1dbbfeeceb037673c9d0266d2cd8ba55ec91b1
UNREALBOT12 <91039807+UNREALBOT12@users.noreply.github.com>
2021-12-22 10:22:07 +0530

    Update global_bans.py

--- aries/modules/global_bans.py
+++ aries/modules/global_bans.py
@@ -554,7 +554,7 @@ dispatcher.add_handler(UNGBAN_HANDLER)
 dispatcher.add_handler(GBAN_LIST)
 dispatcher.add_handler(GBAN_STATUS)
 
-__mod_name__ = "🔘 Anti-Spam"
+__mod_name__ = "ᴀɴᴛɪ-sᴘᴀᴍ"
 __handlers__ = [GBAN_HANDLER, UNGBAN_HANDLER, GBAN_LIST, GBAN_STATUS]
 
 if STRICT_GBAN:  # enforce GBANS if this is set
