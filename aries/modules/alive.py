e2a6cfcd864fa3cc970a52b66906be823d871274
CrePavan <93823376+CrePavan@users.noreply.github.com>
2021-12-22 08:41:00 +0530

    Update alive.py

--- aries/modules/alive.py
+++ aries/modules/alive.py
@@ -3,29 +3,29 @@ from telethon import Button
 from aries import telethn as tbot
 from aries.events import register
 
-PHOTO = "https://telegra.ph/file/b4704fdf82e7a46cc5b51.jpg"
+PHOTO = "https://telegra.ph/file/c20a6da05abc7a15297d1.jpg"
 
 
 @register(pattern=("/alive|/ALIVE"))
 async def awake(event):
     event.sender.first_name
-    ARIES = "**Hello im Saint Aries** \n\n"
+    ARIES = "**Hello im Creator Pavan** \n\n"
     ARIES += "**ALL SYSTEM WORKING PROPERLY**\n\n"
-    ARIES += " ☬ ⌊ **Python :** __3.9.7__ ⌉\n\n"
-    ARIES += " ☬ ⌊ **Pyrogram :** __1.2.9__ ⌉\n\n"
-    ARIES += " ☬ ⌊ **MongoDB :** __2.5.1__ ⌉\n\n"
-    ARIES += " ☬ ⌊ **Platform :** __linux__ ⌉\n\n"
-    ARIES += " ☬ ⌊ **My Lord** : [Artezid](https://t.me/{IDZ}) ☠⌉\n\n"
-    ARIES += " ☬ ⌊ **Sαιητ λrιεs** ⌉\n\n"
-    ARIES += " ☬ ⌊ **TELETHON : 6.6.6 Latest** ⌉\n\n"
+    ARIES += " ⎇ **Python :** __3.9.7__ \n\n"
+    ARIES += " ⎇ **Pyrogram :** __1.2.9__ \n\n"
+    ARIES += " ⎇ **MongoDB :** __2.5.1__ \n\n"
+    ARIES += " ⎇ **Platform :** __linux__ \n\n"
+    ARIES += " ⎇ **My Lord** : [Creator Pavan](https://t.me/Creator_pavan) ☠⌉\n\n"
+    ARIES += " ⎇ **Creator Pavan Robot** ⌉\n\n"
+    ARIES += " ⎇ **TELETHON : 6.6.6 Latest** ⌉\n\n"
     ARIES += " |||| || ||| |||| || |||||| ||||| || || ||"
     BUTTON = [
         [
-            Button.url("Support", "https://t.me/IDZEROIDSUPPORT"),
-            Button.url("Owner", "https://t.me/IdzXartez"),
+            Button.url("Support", "https://t.me/CreatorPavanSupport"),
+            Button.url("Owner", "https://t.me/Creator_Pavan"),
         ]
     ]
     await tbot.send_file(event.chat_id, PHOTO, caption=ARIES, buttons=BUTTON)
 
 
-__mod_name__ = "Alive"
+__mod_name__ = "ᴀʟɪᴠᴇ"
